a34848be4f658cc3ab0a56acf5c02fa11155fde3
Juan Sebastian Rivera Giraldo <jsr3463@rit.edu>
2021-12-21 12:41:00 -0500

    Created Initial Database

---
+++ BugTrackerWeb/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using BugTrackerWeb.Data;
+using BugTrackerWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTrackerWeb.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProjectController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Project> objProjectList = _db.Projects;
+            return View(objProjectList);
+        }
+    }
+}
---
+++ BugTrackerWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BugTrackerWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTrackerWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
